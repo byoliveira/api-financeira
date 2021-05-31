20b5a96beb2cd949f402836efb0e51bd050614d8
Bruno Oliveira <49489698+byoliveira@users.noreply.github.com>
2021-05-30 21:32:01 -0300

    Commit Funcionalidades
    Cadastro de contas
    Validando CPF
    Listando CPF
    Validando conta
    Middlewares

---
+++ api/src/index.js
@@ -0,0 +1,67 @@
+const { request, response } = require("express");
+const { v4: uuidv4} = require("uuid");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+function verifyExistsAccountCPF( request, response, next) {
+     const { cpf } = request.headers;
+
+    const customer = customers.find(customer => customer.cpf ===cpf);
+
+    if(!customer) {
+        return response.status(400).json({ error: "Customer not found"});
+    }
+
+    request.customer  = customer
+    return next();
+}
+
+app.post("/account", (request, response) => {
+    const {cpf, name} = request.body;
+
+    const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({ error: "Customer already exists!"});
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: [],
+    });
+
+    return response.status(201).send();
+});
+
+//app.use(verifyExistsAccountCPF);
+
+app.get("/statement", verifyExistsAccountCPF, (request, response) => {
+    const { customer } = request
+    return response.json(customer.statement);
+});
+
+app.post("/deposit", verifyExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
+
+app.listen(3333);
